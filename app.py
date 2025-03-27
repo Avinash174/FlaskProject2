c09443b08829cb341c775f8661946a4164d0c02d
Avinash Magar <avinashmagar15@gmail.com>
2025-03-27 10:14:14 +0530

    new code added

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome to my project'
+    return 'Welcome to my project !'
+
+@app.route('/hello')
+def hello():
+    return 'Hello All'
+
+# Building Url Dynamically
+
+# success link
+
+@app.route('/success/<int:score>')
+def success(score):
+    return 'The person is passed the marks is :'+str(score)
+
+#failed link
+
+@app.route('/failed/<int:score>')
+def fail(score):
+    return 'The person is failed the marks is :'+str(score)
+
+#result link 
+
+@app.route('/result/<int:score>')
+def result(score):
+    result=''
+    if score<40:
+        result="Failed with marks "+ str(score)
+    else:
+         result="Pass with marks "+ str(score)
+    return result
+        
+   
+
+# variable rule & URL Building
+
+
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
